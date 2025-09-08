e5d7f596d8cc497f4d2a2a5b0fde3090b0a5ef5e
mmaioli <michellecaobianco@gmail.com>
2025-09-08 16:36:50 +0200

    code to extract number of words added

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ def get_book_text():
     with open("books/frankenstein.txt", "r") as file:
         return file.read()
     
-print(get_book_text())
+def get_number_of_words(text):
+    words = text.split()
+    print(len(words))
+
+book_text = get_book_text()
+get_number_of_words(book_text)
