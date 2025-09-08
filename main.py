77914380f527cc0e437a120f5a3697a66611644b
mmaioli <michellecaobianco@gmail.com>
2025-09-08 18:57:14 +0200

    books added, code changed to include sys argv

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+import sys
 from stats import get_number_of_words
 from stats import get_number_of_characters
 from stats import sort_on
 
-def get_book_text():
-    with open("books/frankenstein.txt", "r") as file:
+
+def get_book_text(path):
+    with open(path, "r") as file:
         return file.read()
-    
+
+if len(sys.argv) < 2:
+    print('Usage: python3 main.py "books/"')
+    sys.exit(1)    
+
+book_path = sys.argv[1] 
 
 print("============ BOOKBOT ============")
-print("Analyzing book found at books/frankenstein.txt...")
+print("Analyzing book found at {book_path}...")
 print("----------- Word Count ----------")
-book_text = get_book_text()
+book_text = get_book_text(book_path)
 get_number_of_words(book_text)
 print("-------- Character Count --------")
 sorted_chars = sort_on(get_number_of_characters(book_text))
