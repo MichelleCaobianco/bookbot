e90e208c4b77d4348881c6a9f86a689594433015
mmaioli <michellecaobianco@gmail.com>
2025-09-08 18:01:41 +0200

    sort dict code added

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+from stats import get_number_of_words
+from stats import get_number_of_characters
+from stats import sort_on
+
 def get_book_text():
     with open("books/frankenstein.txt", "r") as file:
         return file.read()
     
 
-from stats import get_number_of_words
+
 book_text = get_book_text()
 get_number_of_words(book_text)
+char_count = get_number_of_characters(book_text)
+
+sorted_chars = sort_on(char_count)
+for char, count in sorted_chars:
+    print(f"'{char}': {count}")
--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
 def get_number_of_words(text):
     words = text.split()
     print(len(words),"words found in the document")
+
+def get_number_of_characters(text):
+    text = text.lower()
+    char_count = {}
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    print(char_count)  # <-- Move this line up  
+    return char_count
+    
+def sort_on(char_count):
+    return sorted(char_count.items(), key=lambda item: item[1], reverse=True)
